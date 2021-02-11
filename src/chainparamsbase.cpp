d1a97881ca8b9455b4b1188803a03ec0a824dc4c
blockinator <36169687+blockinator@users.noreply.github.com>
2021-02-11 12:33:39 -0800

    Update RPC nodes

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -34,11 +34,11 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 21121);
+        return MakeUnique<CBaseChainParams>("", 29051);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet4", 31121);
+        return MakeUnique<CBaseChainParams>("testnet4", 39051);
     else if (chain == CBaseChainParams::REGTEST)
-        return MakeUnique<CBaseChainParams>("regtest", 20443);
+        return MakeUnique<CBaseChainParams>("regtest", 19443);
     else
         throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
