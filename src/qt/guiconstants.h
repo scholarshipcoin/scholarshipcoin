284d61179d3869e38a3c3cba0fb1199a60fdf2f2
Original-Tasty <quentin.neveu@hotmail.ca>
2022-02-21 11:21:53 -0500

    icons + colors updated

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -22,7 +22,7 @@ static const bool DEFAULT_SPLASHSCREEN = true;
 /* Transaction list -- unconfirmed transaction */
 #define COLOR_UNCONFIRMED QColor(128, 128, 128)
 /* Transaction list -- negative amount */
-#define COLOR_NEGATIVE QColor(255, 0, 0)
+#define COLOR_NEGATIVE QColor(48, 38, 126)
 /* Transaction list -- bare address (without label) */
 #define COLOR_BAREADDRESS QColor(140, 140, 140)
 /* Transaction list -- TX status decoration - open until date */
@@ -30,7 +30,7 @@ static const bool DEFAULT_SPLASHSCREEN = true;
 /* Transaction list -- TX status decoration - danger, tx needs attention */
 #define COLOR_TX_STATUS_DANGER QColor(200, 100, 100)
 /* Transaction list -- TX status decoration - default color */
-#define COLOR_BLACK QColor(0, 0, 0)
+#define COLOR_BLACK QColor(60, 60, 60)
 
 /* Tooltips longer than this (in characters) are converted into rich text,
    so that they can be word-wrapped.
